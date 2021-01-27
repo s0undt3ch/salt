b94a1e47d0bf114a57a9026a394e51c0aa4aac79
Pedro Algarvio <pedro@algarvio.me>
2021-01-27 12:16:48 +0000

    Address the security issues flagged by bandit

--- salt/utils/templates.py
+++ salt/utils/templates.py
@@ -444,9 +444,11 @@ def render_jinja_tmpl(tmplstr, context, tmplpath=None):
         opt_jinja_env_helper(opt_jinja_env, "jinja_env")
 
     if opts.get("allow_undefined", False):
-        jinja_env = jinja2.Environment(**env_args)
+        jinja_env = jinja2.Environment(autoescape=True, **env_args)
     else:
-        jinja_env = jinja2.Environment(undefined=jinja2.StrictUndefined, **env_args)
+        jinja_env = jinja2.Environment(
+            undefined=jinja2.StrictUndefined, autoescape=True, **env_args
+        )
 
     tojson_filter = jinja_env.filters.get("tojson")
     indent_filter = jinja_env.filters.get("indent")
